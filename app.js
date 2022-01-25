e2019828ba500996b72ed5e2689e77a383fc7e17
heiio <heiio369@naver.com>
2022-01-25 15:34:52 +0900

    fistt commit2

---
+++ app.js
@@ -0,0 +1,25 @@
+const calculator = {
+  plus: function (a, b) {
+    alert(a + b);
+  },
+  minus: function (a, b) {
+    alert(a - b);
+  },
+  divide: function (a, b) {
+    alert(a / b);
+  },
+  times: function (a, b) {
+    alert(a * b);
+  },
+  power: function (a, b) {
+    alert(a ** b);
+  },
+};
+
+calculator.plus(5, 2);
+
+calculator.minus(5, 2);
+
+calculator.divide(5, 2);
+
+calculator.power(5, 2);
